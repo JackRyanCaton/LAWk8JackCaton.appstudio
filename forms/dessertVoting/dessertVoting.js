fb96b10f5d5ed155d45db802f15973242b1c2fea
JackRyanCaton <jrc88858@creighton.edu>
2021-10-28 22:53:58 -0500

    Initial commit

---
+++ forms/dessertVoting/dessertVoting.js
@@ -0,0 +1,24 @@
+let dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
+                  'Keyline Pie','Cherry Cobbler','Chocolate Chip Cookies']
+
+dessertVoting.onshow=function(){
+    drpDesserts.clear()   
+  for  (i = 0; i < dessertList.length; i++)
+        drpDesserts.addItem(dessertList[i])
+}
+
+
+drpDesserts.onclick=function(s){
+    if (typeof(s) == "object")   
+      return                    
+    else {
+      let message = ""
+      drpDesserts.value = s
+      message = `You picked ${s} - that is a great choice!`
+      lblOutput.value = message
+    }
+}
+
+btnToDescribeYou.onclick=function(){
+  ChangeForm(describeYou)
+}
